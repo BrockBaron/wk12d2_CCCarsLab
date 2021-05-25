ec13f48b23b933353106be0450bff6787583938b
BrockBaron <Nslekkas@gmail.com>
2021-05-25 14:40:42 +0100

    Classes setup

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Customer {
+    private double wallet;
+
+    private ArrayList<Vehicle> vehicles;
+
+    public Customer(double wallet, ArrayList<Vehicle> vehicles) {
+        this.wallet = wallet;
+        this.vehicles = vehicles;
+    }
+
+    public double getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(double wallet) {
+        this.wallet = wallet;
+    }
+
+    public ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+
 }
--- src/main/java/Dealership.java
+++ src/main/java/Dealership.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Dealership {
+    private double till;
+
+    private ArrayList<Vehicle> vehicles;
+
+    public Dealership(double till, ArrayList<Vehicle> vehicles) {
+        this.till = till;
+        this.vehicles = vehicles;
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+    public ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setTill(double till) {
+        this.till = till;
+    }
 }
